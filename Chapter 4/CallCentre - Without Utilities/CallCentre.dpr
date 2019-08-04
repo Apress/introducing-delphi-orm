9651bd5a18cd373f8f51d9501f49645f0f80aef5
John Kouraklis <john.kouraklis@nusenvision.com>
2019-08-04 13:01:16 +0100

    + chapter 4

---
+++ Chapter 4/CallCentre - Without Utilities/CallCentre.dpr
@@ -0,0 +1,23 @@
+program CallCentre;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  MainForm in 'Forms\MainForm.pas' {FormMain},
+  EntityForm in 'Forms\EntityForm.pas' {FormEntity},
+  Entities in 'Entities.pas',
+  ConnectionModule in 'Forms\ConnectionModule.pas' {SQLiteConnection: TDataModule},
+  Database.Session in 'Database.Session.pas',
+  Database.Session.Types in 'Database.Session.Types.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TFormMain, FormMain);
+  Application.CreateForm(TSQLiteConnection, SQLiteConnection);
+  Application.CreateForm(TSQLiteConnection, SQLiteConnection);
+  Application.Run;
+end.
+
+
